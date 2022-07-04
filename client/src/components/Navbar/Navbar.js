6cd809cdc8c65fb389d57fbb0e85f73e66576bdb
tatamibox <tat4mi@gmail.com>
2022-07-04 14:06:28 -0700

    create navbar + navbar links

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,8 +5,30 @@ const Navbar = () => {
 
 
     return (
-     <nav className="text-center mt-3 sticky-top"><img class="logo" src={logo}></img></nav>
-     
+        <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid d-flex align-items-center my-2">
+        <a class="navbar-brand brandName" href="#">tat.cx</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                
+            </ul>
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sign up</a>
+                </li>
+                <li class="nav-item farRight py-1 px-3">
+                    <a class="nav-link" href="#">Log in</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
     )
 }
 
