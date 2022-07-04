543cc3f192e90f314abc47fdba2b956796356afc
tatamibox <tat4mi@gmail.com>
2022-07-04 12:14:30 -0700

    add placeholder logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import React, {useEffect, useState} from 'react'
-
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
+    <body>
+    <Navbar></Navbar>
+    </body>
+  )
+
 }
 
 export default App
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,13 @@
+
+import logo from '../../assets/img/logo.png'
+import './Navbar.css'
 const Navbar = () => {
 
+
+    return (
+     <nav className="text-center mt-3 sticky-top"><img class="logo" src={logo}></img></nav>
+     
+    )
 }
 
 export default Navbar;
