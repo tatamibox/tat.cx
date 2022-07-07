e820873dff8e2683d3364965403d1284f308426c
tatamibox <tat4mi@gmail.com>
2022-07-07 11:38:29 -0700

    background color

--- client/src/Routes/Home.js
+++ client/src/Routes/Home.js
@@ -2,18 +2,18 @@ import Navbar from "../components/Navbar/Navbar";
 import Infographic from "../components/Infographic/Infographic";
 import Pitch from "../components/Pitch/Pitch";
 const Home = () => {
-  
-    return (
-      <body>    
+
+  return (
+    <body className="home">
       <Navbar></Navbar>
       <Infographic></Infographic>
       <Pitch></Pitch>
-   
-      </body>
-     
-        
-    )
-  
-  }
-  
-  export default Home;
+
+    </body>
+
+
+  )
+
+}
+
+export default Home;
