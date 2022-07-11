ad7287df790e7c759b95efa33eda4831a931ca7b
tatami <106715069+tatamibox@users.noreply.github.com>
2022-07-11 08:57:19 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,12 @@ app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
 
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
