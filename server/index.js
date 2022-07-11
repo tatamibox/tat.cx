4286eb0deb2b53af5565636a537590d15ed12f93
tatamibox <tat4mi@gmail.com>
2022-07-10 18:56:43 -0700

    edit serve in server js

--- server/index.js
+++ server/index.js
@@ -24,10 +24,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
 
 
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+}
+
+else {
+    app.use(express.static(path.join(__dirname, '/client/public')));
+    app.get("/*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./client/public/index.html"));
     });
 }
 
