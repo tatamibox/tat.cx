e306637b399183d458eaef5404c6642b3872d4fb
tatamibox <tat4mi@gmail.com>
2022-07-11 09:47:28 -0700

    twaek

--- server/index.js
+++ server/index.js
@@ -24,16 +24,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-}
-
-else {
-    app.use(express.static(path.join(__dirname, '/client/public')));
-    app.get("/*", function (req, res) {
-        res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    // Set the static assets folder (ie, client build)
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 }
 
